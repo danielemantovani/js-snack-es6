6b53d217db485dceeffc56e6203513053dd518a5
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-15 18:27:05 +0200

    feat: snack1 completed

--- Snack1/js/script.js
+++ Snack1/js/script.js
@@ -2,10 +2,6 @@
 //Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 //Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
 
-// estraggo gli oggetti dall'array
-// metto a confronto il peso dei diversi oggetti
-// stampo solo quello che pesa di meno 
-
 // test
 // alert ("Bicycle");
 
@@ -29,11 +25,27 @@ const streetBicycle = [
     
 ];
 
-// console.log(streetBicycle);
+let bikeLessModel = "";
+// inizializzo la variabile con un numero alto così che al confronto con la prima bici risulti la più leggera
+let bikeLessWeight = 999;
 
 streetBicycle.forEach ((curBicycle) => {
-    console.log(curBicycle);
-    // const bikeInfo = { model, weight} = streetBicycle
-    // console.log(bikeInfo);
+    // console.log(curBicycle);
+    const {model, weight} = curBicycle
+    // console.log(model, "il suo peso è di", weight, "kg");
     
+    // se il peso della bici è minore rispetto al peso della bici messa a confronto durante l'esecuzione del ciclo for salva la bici e il peso della bici che è più leggera
+    if (weight < bikeLessWeight) { 
+        bikeLessModel = model;
+        bikeLessWeight = weight;
+    }
 })
+
+console.log( "Modello:", bikeLessModel, "Peso:", bikeLessWeight, "kg");
+
+let printBike = document.getElementById ("light-bike");
+// console.log(printBike);
+
+printBike.innerHTML += `
+La bici più leggera è ${bikeLessModel} con un peso di ${bikeLessWeight} kg
+`
