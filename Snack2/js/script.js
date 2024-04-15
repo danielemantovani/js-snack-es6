c0fae1c4a3ee09808feb1e9c372714fe5f377fe9
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-15 18:58:10 +0200

    feat: creazione array di oggetti squadre di calcio

--- Snack2/js/script.js
+++ Snack2/js/script.js
@@ -1,8 +1,33 @@
-// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
-// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-// Generare numeri random al posto degli 0 nelle proprietà:
-// Punti fatti e falli subiti.
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti
+//Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+
+
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 // test
 // alert ("football team");
+
+const footballTeam = [
+    {
+        teamName : "Inter",
+        foulsRecived : 0,
+        points : 0,
+    },
+    {
+        teamName : "Cosenza",
+        foulsRecived : 0,
+        points : 0,
+    },
+    {
+        teamName : "Parma",
+        foulsRecived : 0,
+        points : 0,
+    },
+    {
+        teamName : "Cagliari",
+        foulsRecived : 0,
+        points : 0,
+    }
+];
+
