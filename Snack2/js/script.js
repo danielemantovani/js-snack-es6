3cc60989e4da3fd3479edc8d070dc4a5dd4af619
danielemantovani <daniele.mantovani124@gmail.com>
2024-04-16 16:16:50 +0200

    feat: snack2 (squadre di calcio) completato

--- Snack2/js/script.js
+++ Snack2/js/script.js
@@ -1,7 +1,5 @@
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-
-
 // Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
 // Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
@@ -30,4 +28,28 @@ const footballTeam = [
         points : 0,
     }
 ];
+
+
+
+//uso forEach e non map in quanto per adesso non è necessario generare un nuovo array
+footballTeam.forEach((curTeam) => {
+    curTeam.foulsRecived = Math.floor(Math.random() * 500);
+    curTeam.points = Math.floor(Math.random() * 200);
+});
+
+console.log(footballTeam);
+
+const newTeam = footballTeam.map ((curTeam) =>{
+    const {teamName , foulsRecived} = curTeam
+    return {teamName , foulsRecived}
+});
+
+console.log(newTeam)
+
+
+
+
+
+
+
 
